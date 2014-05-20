096d06952dd33f6f53be584209d36851ae7ea32a
LuisRBarreras <lbarreras@nearsoft.com>
2014-05-19 18:28:49 -0700

    First commmit

---
+++ backup/rpc.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var Datastore = require('nedb');
+var db = {};
+
+db.movies = new Datastore({ filename: 'db/movies', autoload: true});
+
+app.use(express.bodyParser());
+
+app.get('/', function (req, res) {
+	res.send("The API is working.");
+})
+.post('/rpc',function (req, res) {
+	var body = req.body;
+	var respond = function (err, results) {
+		if (err) {
+			res.send(JSON.stringify(err));
+		} else {
+			res.send(JSON.stringify(results));
+		}
+	};
+
+	res.set('Content-type', 'application/json');
+
+	switch (body.action) {
+		case "getMovies":
+			db.movies.find({}, respond);
+			break;
+
+		case "addMovie":
+			db.movies.insert({ title: body.title}, respond);
+			break;	
+
+		case "rateMovie":
+			db.movies.update({ title: body.title }, {
+				$set: { rating: body.rating } 
+			}, function (err,num) {
+				respond(err, { success: num + " records updated" });
+
+			});
+			break;
+			
+
+		default:
+			res.send("No action gives");
+	}
+
+})
+.listen(2500);
---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var Datastore = require('nedb');
+var db = {};
+var responder = require('./httpResponder');
+
+//Connect to an NeDB database
+db.movies = new Datastore({ filename: 'db/movies', autoload: true});
+
+//Necessary for accesing POST data via req.body object
+app.use(express.bodyParser());
+
+
+//Catch-all route to set global values
+app.use(function (req, res, next){
+	res.type('application/json');
+	res.locals.respond = responder.setup(res);
+	next();
+});
+
+// Routes
+app.get('/', function (req, res) {
+	res.send("The API is working.");
+})
+
+app.get('/movies', function (req, res) {
+	db.movies.find({}, res.locals.respond);
+});
+
+app.post('/movies', function (req, res) {
+	db.movies.insert({ title: req.body.title}, res.locals.respond);
+});
+
+
+app.get('/movies/:id', function (req, res) {
+	db.movies.findOne({ _id: req.params.id}, res.locals.respond);
+});
+
+app.put('/movies/:id' , function (req, res) {
+	db.movies.update({ _id: req.params.id}, req.body, function (err, num) {
+		res.locals.respond(err, { success: num + " records updated" });
+	})
+});
+
+app.delete('/movies/:id', function (req, res) {
+	db.movies.remove({ _id: req.params.id}, function (err, num) {
+		res.locals.respond(err, { success: num + " record deleted"});
+	});
+});
+
+app.listen(2500);
